878f1e7aacdffe23ecfb24dfda161667cc4bda3e
bab21 <2016061@iiitdmj.ac.in>
2020-11-01 20:03:54 +0530

    refactored code for get contact by city name

--- src/test/java/com/capgemini/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/capgemini/addressbook/AddressBookServiceTest.java
@@ -9,31 +9,37 @@ import org.junit.Test;
 
 public class AddressBookServiceTest {
 
-//	@Test
-//	public void givenDatabase_readContacts_ShouldMatch() {
-//		AddressBook addressBook=new AddressBook();
-//		List<Contact> contactList=addressBook.readContacts();
-//		assertEquals(3,contactList.size());
-//	}
-//	
-//	@Test
-//	public void givenContact_WhenUpdated_ShouldSync() throws SQLException {
-//		AddressBook addressBook=new AddressBook();
-//		addressBook.editContact("Nancy");
-//		List<Contact> contactList=addressBook.readContacts();
-//		System.out.println(contactList);
-//		boolean ans=addressBook.checkAddressBookDataInSyncWithDB("Nancy");
-//		assertTrue(ans);
-//	}
+	@Test
+	public void givenDatabase_readContacts_ShouldMatch() {
+		AddressBook addressBook=new AddressBook();
+		List<Contact> contactList=addressBook.readContacts();
+		assertEquals(3,contactList.size());
+	}
+	
+	@Test
+	public void givenContact_WhenUpdated_ShouldSync() throws SQLException {
+		AddressBook addressBook=new AddressBook();
+		addressBook.editContact("Nancy");
+		List<Contact> contactList=addressBook.readContacts();
+		System.out.println(contactList);
+		boolean ans=addressBook.checkAddressBookDataInSyncWithDB("Nancy");
+		assertTrue(ans);
+	}
 	@Test
 	public void givenContact_WhenDeleted_SHouldMatch() {
 		AddressBook addressBook=new AddressBook();
 		List<Contact> contactList=addressBook.readContacts();
 		System.out.println("Data list size before deletion"+contactList.size());
-		addressBook.deleteContact("Babli");
+		addressBook.deleteContact("Nancy");
 		contactList=addressBook.readContacts();
 		System.out.println("Size after deletion"+contactList.size());
 		
+	}
+	@Test
+	public void givenAddressBookId_getContactsSortedByCity_ShouldMatch() {
+		AddressBook addressBook=new AddressBook();
+		List<Contact> contactList=addressBook.getSortedContactListByCity(4);
+		assertTrue(contactList.get(0).getCity().equals("Kolkata"));
 	}
 
 }
