7841e53fdb9f45eac5441352fc6dbdfbe46e913b
bab21 <2016061@iiitdmj.ac.in>
2020-11-01 17:21:18 +0530

    connect to database and tested

--- src/test/java/com/capgemini/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/capgemini/addressbook/AddressBookServiceTest.java
@@ -9,10 +9,11 @@ import org.junit.Test;
 public class AddressBookServiceTest {
 
 	@Test
-	public void test() {
+	public void givenDatabase_readContacts_ShouldMatch() {
 		AddressBook addressBook=new AddressBook();
 		List<Contact> contactList=addressBook.readContacts();
 		assertEquals(3,contactList.size());
 	}
+	
 
 }
